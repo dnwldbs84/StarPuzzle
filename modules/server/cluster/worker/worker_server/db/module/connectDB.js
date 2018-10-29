fea9c22da4a5966384ef9e32fa87602dd7e5d8dd
dnwldbs84 <31395554+dnwldbs84@users.noreply.github.com>
2018-10-29 17:45:01 +0900

    w

--- modules/server/cluster/worker/worker_server/db/module/connectDB.js
+++ modules/server/cluster/worker/worker_server/db/module/connectDB.js
@@ -3,14 +3,20 @@
 var setting = {
   socketPath : '/var/run/mysqld/mysqld.sock',
   host: 'localhost',
-  user: 'root',
-  password: 'dbs34225277',
+  user: '',
+  password: '',
   database: 'world',
 
   dateStrings: 'date'
 };
 
 exports.connect = function(mysql) {
+  var fs = require('fs');
+  var text = fs.readFileSync('setting.txt');
+  var datas = text.split(',');
+  setting.user = datas[0];
+  setting.password = datas[1];
+
   conn = mysql.createConnection(setting);
   conn.connect();
   return conn;
