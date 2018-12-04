6f36f147537b8f66d7d58a9922de694f5a5ba1a9
dnwldbs84 <31395554+dnwldbs84@users.noreply.github.com>
2018-12-04 10:36:15 +0900

    Update worker.js

--- modules/server/cluster/worker/worker.js
+++ modules/server/cluster/worker/worker.js
@@ -260,6 +260,7 @@ exports.initWorker = function(port) {
             userDB.findById(client.uid, (err, result) => {
                 var myRating = result.rating;
                 userDB.findById(oppAIId, (err, oppResult) => {
+                  if (oppResult) {
                     var oppRating = oppResult.rating;
                     var newRating = publicModule.publicFunctions.calcRating(oppRating, myRating, true);
                     // var newRating = myRating - (16 + (myRating - oppRating) * (16 / 400));
@@ -268,6 +269,7 @@ exports.initWorker = function(port) {
                     // userDB.updateUserMultiData(client.uid, 'pvpLoseCount', result.pvpLoseCount + 1,
                     //                                        'rating', newRating);
                   });
+                }
               });
           }
           break;
